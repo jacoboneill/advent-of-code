4179bc20d783fcc6313968063120a5bb56b36e93
Jacob O'Neill <jacoboneill2000@outlook.com>
2023-12-05 14:26:04 +0000

    Complete Part 2

--- 2023/01/main.cpp
+++ 2023/01/main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 #include <fstream>
+#include <map>
 
 class Parser{
 public:
   Parser(std::string file_location) : 
-    m_fl(file_location)
+    m_fl(file_location),
+    m_map({
+      {"zero", 0},
+      {"one", 1},
+      {"two", 2},
+      {"three", 3},
+      {"four", 4},
+      {"five", 5},
+      {"six", 6},
+      {"seven", 7},
+      {"eight", 8},
+      {"nine", 9}
+    })
     {}
 
   int parse(){
     std::string line;
     std::fstream file(m_fl);
-    int count;
+    int count = 0;
 
     while(std::getline(file, line)){
-      int line_total = searchFront(line) + searchBack(line); 
+      int line_total = (searchFront(line) * 10) + searchBack(line); 
       count += line_total;
       std::cout << line << " | " << line_total << " | " << count << std::endl;
     }
@@ -26,7 +39,15 @@ private:
   int searchFront(std::string line){
     for(int i = 0; i < line.size(); i++){
       if(isdigit(line[i])){
-        return 10 * (line[i] - '0');
+        return line[i] - '0';
+      }
+
+      for(auto it : m_map){
+        if(line[i] == it.first[0]){
+          if(line.substr(i, it.first.size()) == it.first){
+            return it.second;
+          }
+        }
       }
     }
 
@@ -38,12 +59,22 @@ private:
       if(isdigit(line[i])){
         return line[i] - '0';
       }
+
+      for(auto it : m_map){
+        if(line[i] == it.first[it.first.size() - 1]){
+          if(i - static_cast<int>(it.first.size()) + 1 >= 0){ // Have to static cast as size is a unsigned size_t
+            if(line.substr(i - it.first.size() + 1, it.first.size()) == it.first){
+              return it.second;
+            }
+          }
+        }
+      }
     }
 
     return -1;
   }
-
   std::string m_fl;
+  std::map<std::string, int> m_map;
 };
 
 int main (int argc, char *argv[]) {
