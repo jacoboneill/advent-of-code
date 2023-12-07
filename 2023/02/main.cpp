1bd41fb9c1f3d067abd11582815b706a344bb5f1
Jacob O'Neill <jacoboneill2000@outlook.com>
2023-12-07 09:54:41 +0000

    Add part 2

--- 2023/02/main.cpp
+++ 2023/02/main.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <fstream>
 #include <iostream>
 #include <map>
@@ -24,19 +25,28 @@ public:
 
   int count(){
     std::string line;
-    int possible_id_sum = 0;
+    int result = 0;
 
     while(std::getline(m_fs, line)){
-     possible_id_sum += parseLine(line);
+      ParsedLine parsed_line = parseLine(line);
+      // std::cout << parsed_line.id << ": " << (part1(parsed_line) != 0 ? "Valid" : "Invalid") << std::endl;
+      // result += part1(parsed_line);
+      std::cout << parsed_line.id << ": " << part2(parsed_line) << std::endl;
+      result += part2(parsed_line);
     }
     
     // possible_id_sum += parseLine("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
-    return possible_id_sum;
+    return result;
   }
 
 private:
   std::ifstream m_fs;
   std::map<std::string, int> m_valid_color_map;
+
+  struct ParsedLine{
+    int id;
+    std::vector<std::vector<std::string>> plays;
+  };
 
   std::string printVector(std::vector<bool> vector){
     std::string out;
@@ -87,6 +97,18 @@ private:
 
     return out;
   }
+
+  std::string printMap(std::map<std::string, int> map){
+    std::vector<std::vector<std::string>> converted_map;
+    for(auto it : map){
+      std::vector<std::string> key_value;
+      key_value.push_back(it.first);
+      key_value.push_back(std::to_string(it.second));
+      converted_map.push_back(key_value);
+    }
+
+    return print2DVector(converted_map);
+  }
   
   std::vector<std::string> split(std::string input, std::string delimiter){
     std::vector<std::string> result;
@@ -103,12 +125,11 @@ private:
     return result;
   }
 
-  int parseLine(std::string line){
-    std::cout << line << std::endl;
+  ParsedLine parseLine(std::string line){
+    // std::cout << line << std::endl;
 
     // Split string and get ID
     int id = std::stoi(line.substr(line.find(' ') + 1, line.find(": ") - line.find(' ') - 1));
-    // std::cout << id << std::endl;
 
     // Split ID from Games
     std::string games = line.substr(line.find(": ") + 2);
@@ -125,24 +146,41 @@ private:
       plays.push_back(play);
     }
 
-    // Split Plays into Entries of Number and Color
-    std::vector<bool> valid_plays;
-    for(std::vector<std::string> play : plays){
+    return {id, plays};
+  }
+
+  int part1(ParsedLine line){
+    // Loop through
+    for(std::vector<std::string> play : line.plays){
       for(std::string p : play){
         std::vector<std::string> split_p = split(p, " ");
-        valid_plays.push_back(std::stoi(split_p[0]) <= m_valid_color_map[split_p[1]]);
+        if(std::stoi(split_p[0]) > m_valid_color_map[split_p[1]]){
+          return 0; 
+        }
       }
     }
 
-    std::cout << printVector(valid_plays) << std::endl;
+    return line.id;
+  }
 
-    // Check if any are not valid Plays
-    for(bool valid_play : valid_plays){
-      if(!valid_play){
-        return 0;
+  int part2(ParsedLine line){
+    std::map<std::string, int> color_map = {
+      {"red", 0},
+      {"green", 0},
+      {"blue", 0}
+    };
+
+    for(std::vector<std::string> play : line.plays){
+      for(std::string p : play){
+        std::vector<std::string> split_p = split(p, " ");
+        if(color_map[split_p[1]] < std::stoi(split_p[0])){
+          color_map[split_p[1]] = std::stoi(split_p[0]);
+        }
       }
     }
-    return id;
+
+    int product = color_map["red"] * color_map["green"] * color_map["blue"];
+    return product;
   }
 };
 
