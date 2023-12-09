4c79fa6b4fac078e949a7dc393ed214543ed92d2
Jacob O'Neill <jacoboneill2000@outlook.com>
2023-12-09 17:51:31 +0000

    Update template main.cpp

---
+++ 2023/template/main.cpp
@@ -0,0 +1,39 @@
+#include <fstream>
+#include <iostream>
+#include "../utils/print.hpp"
+#include "../utils/string.hpp"
+
+class Day{
+public:
+  Day(std::string file_location){
+    m_fs.open(file_location);
+    if(!m_fs.is_open()){
+      std::cerr << "Error, can't open: " << file_location << std::endl;
+    }
+  }
+
+  ~Day(){
+    m_fs.close();
+  }
+
+  int* play(){
+    new int[2] answers;
+    
+    while(std::getline(m_fs, line){
+      // ParseLine
+      // Update answers[0]
+      // Update answers[1]
+    }
+
+    return answers;
+  }
+
+private:
+  std::ifstream m_fs;
+};
+
+int main (int argc, char *argv[]) {
+  Day day;
+
+  return 0;
+}
